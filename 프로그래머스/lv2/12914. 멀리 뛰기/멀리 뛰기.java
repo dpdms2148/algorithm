0afa3a02451653f2b6704b59f57d64be74ea78db
dpdms2148 <57666226+dpdms2148@users.noreply.github.com>
2023-02-21 11:01:23 +0900

    [level 2] Title: 멀리 뛰기, Time: 0.10 ms, Memory: 75.5 MB -BaekjoonHub

---
+++ 프로그래머스/lv2/12914. 멀리 뛰기/멀리 뛰기.java
@@ -0,0 +1,20 @@
+class Solution {
+    public long solution(int n) {
+        long answer = 0;
+        
+        // 경우의 수를 저장하는 배열 초기화
+        long[] dp = new long[n];
+        dp[0] = 1;
+        if( n== 1){
+            return 1;
+        }
+        dp[1] = 2;
+        // i칸에 도착하는 경우의 수
+        // = (i-1)칸에서 1칸 뛰어서 오기 + (i-2)칸에서 2칸 뛰어오기
+        for(int i = 2; i < n; i++){
+            dp[i] = (dp[i - 1] + dp[i - 2]) % 1234567;
+        }
+        answer =  dp[n - 1];
+        return answer;
+    }
+}
