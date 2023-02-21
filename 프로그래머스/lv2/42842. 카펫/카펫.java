1b31c858e787d6aeab6a280f7f08006edded89b5
dpdms2148 <57666226+dpdms2148@users.noreply.github.com>
2023-02-21 09:13:14 +0900

    [level 2] Title: 카펫, Time: 0.02 ms, Memory: 66.2 MB -BaekjoonHub

--- 프로그래머스/lv2/42842. 카펫/카펫.java
+++ 프로그래머스/lv2/42842. 카펫/카펫.java
@@ -1,13 +1,18 @@
 class Solution {
     public int[] solution(int brown, int yellow) {
         int[] answer = new int[2];
+        //전체 면적
         int sum = brown + yellow;
         
-        for(int i = 3; i < sum; i++){
+        // i : 세로 길이
+        for(int i = 3; i < sum; i++) {
+            // j : 가로 길이
             int j = sum  / i;
             
-            if(sum % i == 0 && j >= 3){
+            if(sum % i == 0 && j >= 3) {
+                // yellow 세로 길이
                 int width = Math.max(i, j);
+                // yellow 가로 길이
                 int height = Math.min(i, j);
                 if(yellow == (width - 2) * (height - 2)){
                     answer[0] = width;
