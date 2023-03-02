42a6730435a6e090fe2f290076f3fde356565503
dpdms2148 <57666226+dpdms2148@users.noreply.github.com>
2023-03-02 23:06:01 +0900

    [level 2] Title: [1차] 캐시, Time: 2.71 ms, Memory: 81.5 MB -BaekjoonHub

---
+++ 프로그래머스/lv2/17680. ［1차］ 캐시/［1차］ 캐시.java
@@ -0,0 +1,37 @@
+import java.util.ArrayList;
+
+class Solution {
+    public int solution(int cacheSize, String[] cities) {
+        int answer = 0;
+        int n = cities.length;
+        ArrayList<String> cache = new ArrayList<>();
+        
+        //캐시 사이즈가 0이라면 아무것도 저장할 수 없으므로 바로 종료
+        if(cacheSize == 0){
+            return n * 5;
+        }
+        for(int i = 0; i < n; i++){
+            cities[i] = cities[i].toUpperCase();
+        }
+        
+        for(int i = 0; i < n; i++) {
+            //캐시에 있는 경우
+            if(cache.contains(cities[i])) {
+                answer += 1;
+                //해당 도시 캐시 가장 처음으로 이동
+                cache.remove(cities[i]);
+            }
+            //캐시에 없는 경우
+            else {
+                answer += 5;
+                //캐시가 가득찬 경우
+                if(cache.size() >= cacheSize){
+                    cache.remove(cacheSize - 1);
+                }
+            }
+            cache.add(0, cities[i]);
+        }
+        
+        return answer;
+    }
+}
