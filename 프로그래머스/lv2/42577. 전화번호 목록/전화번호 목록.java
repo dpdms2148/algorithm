8613ddd4ecfe8fa5d40ad9d99f1ece7fb68cdafa
dpdms2148 <57666226+dpdms2148@users.noreply.github.com>
2023-03-07 00:07:40 +0900

    [level 2] Title: 전화번호 목록, Time: 269.05 ms, Memory: 142 MB -BaekjoonHub

--- 프로그래머스/lv2/42577. 전화번호 목록/전화번호 목록.java
+++ 프로그래머스/lv2/42577. 전화번호 목록/전화번호 목록.java
@@ -1,25 +1,48 @@
 import java.util.Arrays;
+import java.util.HashMap;
+import java.util.Map;
 
 class Solution {
     public boolean solution(String[] phone_book) {
         boolean answer = true;
-        Arrays.sort(phone_book);
+        Map<String, Integer> map = new HashMap<>();
         int n = phone_book.length;
-        for(int i = 0; i < n - 1; i++){
-            int m = phone_book[i].length();
-            for(int j = 0; j < m; j++){
-                if(phone_book[i+1].charAt(j) != phone_book[i].charAt(j)){
-                    answer = true;
-                    break;
-                }else{
-                    answer = false;
+        
+        for(int i = 0; i < n;  i++){
+            map.put(phone_book[i], i);
+        }
+        for(int i = 0; i < n; i++){
+            for(int j = 0; j<phone_book[i].length(); j++){
+                if(map.containsKey(phone_book[i].substring(0, j))){
+                    return false;
                 }
             }
-            if(!answer){
-                return answer;
-            }
-            
         }
+            
+        // Arrays.sort(phone_book);
+        // for(int i = 0; i < n - 1; i++){
+            
+            
+            //방법2. startWith()사용
+            // if(phone_book[i+1].startsWith(phone_book[i])){
+            //     return false;
+            // }
+            
+            // 방법1. 그냥 반복문 사용
+            // int m = phone_book[i].length();
+            // for(int j = 0; j < m; j++){
+            //     if(phone_book[i+1].charAt(j) != phone_book[i].charAt(j)){
+            //         answer = true;
+            //         break;
+            //     }else{
+            //         answer = false;
+            //     }
+            // }
+            // if(!answer){
+            //     return answer;
+            // }
+            
+        // }
         return answer;
     }
 }
