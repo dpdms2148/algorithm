ad4a6c5c4e0f64673494e8b4d3f1ebf9fc7d2687
dpdms2148 <57666226+dpdms2148@users.noreply.github.com>
2023-03-07 20:37:51 +0900

    [unrated] Title: 귤 고르기, Time: 70.71 ms, Memory: 91.6 MB -BaekjoonHub

--- 프로그래머스/unrated/138476. 귤 고르기/귤 고르기.java
+++ 프로그래머스/unrated/138476. 귤 고르기/귤 고르기.java
@@ -8,22 +8,28 @@ class Solution {
     public int solution(int k, int[] tangerine) {
         Map<Integer, Integer> map = new HashMap<>();
         int answer = 0;
+        
         int n = tangerine.length;
         for(int i = 0; i < n; i++){
+            //맵에 귤 크기가 저장되어 있는 경우
             if(map.containsKey(tangerine[i])){
                 map.put( tangerine[i], map.get(tangerine[i]) + 1 );
-            }else{
+            }
+            //맵에 귤 크기가 저장되어 있지 않은 경우
+            else{
                 map.put( tangerine[i], 1 );
             }
         }
         
+        //귤이 많은 순서대로(내림차순으로) 정렬
         List<Integer> keySet = new ArrayList<>(map.keySet());
         keySet.sort((o1, o2) -> map.get(o2).compareTo(map.get(o1)));
+        
+        //현재 담은 귤의 개수
         int sum = 0;
         for (Integer key : keySet) {
-            // System.out.print("Key : " + key);
-            // System.out.println(", Val : " + map.get(key));
             sum += map.get(key);
+            //귤 개수가 k를 넘은 경우
             if(sum >= k){
                 answer++;
                 break;
