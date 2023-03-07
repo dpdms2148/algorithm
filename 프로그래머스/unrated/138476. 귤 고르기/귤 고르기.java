8d8a133d0989642d25b17896b4c6ce4e576c39b8
dpdms2148 <57666226+dpdms2148@users.noreply.github.com>
2023-03-07 20:41:52 +0900

    [unrated] Title: 귤 고르기, Time: 54.73 ms, Memory: 99.6 MB -BaekjoonHub

--- 프로그래머스/unrated/138476. 귤 고르기/귤 고르기.java
+++ 프로그래머스/unrated/138476. 귤 고르기/귤 고르기.java
@@ -2,33 +2,26 @@ import java.util.HashMap;
 import java.util.Map;
 import java.util.ArrayList;
 import java.util.List;
-import java.util.Arrays;
+import java.util.Collections;
 
 class Solution {
     public int solution(int k, int[] tangerine) {
         Map<Integer, Integer> map = new HashMap<>();
         int answer = 0;
-        
-        int n = tangerine.length;
-        for(int i = 0; i < n; i++){
-            //맵에 귤 크기가 저장되어 있는 경우
-            if(map.containsKey(tangerine[i])){
-                map.put( tangerine[i], map.get(tangerine[i]) + 1 );
-            }
-            //맵에 귤 크기가 저장되어 있지 않은 경우
-            else{
-                map.put( tangerine[i], 1 );
-            }
+
+        for(int i = 0; i < tangerine.length; i++){
+            int count = map.getOrDefault(tangerine[i], 0);
+            map.put(tangerine[i], count+1);
         }
         
         //귤이 많은 순서대로(내림차순으로) 정렬
-        List<Integer> keySet = new ArrayList<>(map.keySet());
-        keySet.sort((o1, o2) -> map.get(o2).compareTo(map.get(o1)));
+        List<Integer> valueList = new ArrayList(map.values());
+        Collections.sort(valueList,Collections.reverseOrder());
         
         //현재 담은 귤의 개수
         int sum = 0;
-        for (Integer key : keySet) {
-            sum += map.get(key);
+        for (Integer val : valueList) {
+            sum += val;
             //귤 개수가 k를 넘은 경우
             if(sum >= k){
                 answer++;
