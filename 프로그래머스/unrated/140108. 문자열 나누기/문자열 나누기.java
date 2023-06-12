c699b6bfd2513b744d8629394ce383e6971063da
dpdms2148 <57666226+dpdms2148@users.noreply.github.com>
2023-06-12 18:00:21 +0900

    [unrated] Title: 문자열 나누기, Time: 0.66 ms, Memory: 76.9 MB -BaekjoonHub

--- 프로그래머스/unrated/140108. 문자열 나누기/문자열 나누기.java
+++ 프로그래머스/unrated/140108. 문자열 나누기/문자열 나누기.java
@@ -1,37 +1,18 @@
 class Solution {
     public int solution(String s) {
         int answer = 0;
+        int sameCount = 0;
+        int diffCount = 0;
+        char x = s.charAt(0);
         
         for(int i = 0; i < s.length(); i++){
-            //기준 문자 및 카운트 초기화
-            char x = s.charAt(i);
-            System.out.println(x+"("+i+")");
-            int sameCount = 1;
-            int diffCount = 0;
-            
-            //남은 부분이 없으므로 종료
-            if(i == s.length() - 1){
+            if(sameCount == diffCount){
                 answer++;
-                break;
+                x = s.charAt(i);    
             }
             
-            //탐색 시작
-            int j = 0;
-            while(sameCount!=diffCount){
-                j++;
-                if((i + j) == s.length() - 1){
-                    break;
-                }
-                
-                if(x == s.charAt(i + j)){
-                    sameCount++;
-                }else{
-                    diffCount++;
-                }
-            }
-            i += j;
-            
-            answer++;
+            if (x == s.charAt(i)) sameCount++;
+            else diffCount++;
         }
         return answer;
     }
