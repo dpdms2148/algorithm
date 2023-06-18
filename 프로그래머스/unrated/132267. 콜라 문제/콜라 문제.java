2ce309b1aab1c9792202bf313e135645eb17059c
dpdms2148 <57666226+dpdms2148@users.noreply.github.com>
2023-06-18 23:23:56 +0900

    [unrated] Title: 콜라 문제, Time: 0.02 ms, Memory: 76.8 MB -BaekjoonHub

--- 프로그래머스/unrated/132267. 콜라 문제/콜라 문제.java
+++ 프로그래머스/unrated/132267. 콜라 문제/콜라 문제.java
@@ -2,7 +2,9 @@ class Solution {
     public int solution(int a, int b, int n) {
         int answer = 0;
         while(n >= a){
+            // 빈병 n개를 가져가서 받은 콜라의 수
             answer += (n / a) * b;
+            // 남은 빈병의 수
             n = (n / a) * b + n % a;
         }
         return answer;
