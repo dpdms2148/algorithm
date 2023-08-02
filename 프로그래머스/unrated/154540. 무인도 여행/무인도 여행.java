66503b290e0ecf7b1a36690e2098fa2e544a544d
dpdms2148 <57666226+dpdms2148@users.noreply.github.com>
2023-08-02 23:54:57 +0900

    [unrated] Title: 무인도 여행, Time: 0.46 ms, Memory: 74.2 MB -BaekjoonHub

--- 프로그래머스/unrated/154540. 무인도 여행/무인도 여행.java
+++ 프로그래머스/unrated/154540. 무인도 여행/무인도 여행.java
@@ -57,7 +57,7 @@ class Solution {
         }else{
             Collections.sort(list);
             answer = new int[list.size()];
-            for(int i=0; i<list.size(); i++){
+            for(int i = 0; i < list.size(); i++){
                 answer[i] = list.get(i);
             }
         }
