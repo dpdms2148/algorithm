a29f41b71f9390c640617223db2232c825ede50e
dpdms2148 <57666226+dpdms2148@users.noreply.github.com>
2024-08-19 22:31:07 +0900

    [Silver II] Title: 생태학, Time: 660 ms, Memory: 88964 KB -BaekjoonHub

--- 백준/Silver/4358. 생태학/생태학.java
+++ 백준/Silver/4358. 생태학/생태학.java
@@ -1,27 +1,48 @@
-import java.util.*;
+import java.io.*;
+import java.util.Map;
+import java.util.TreeMap;
+
 public class Main {
-    
-    public static void main(String[] args) {
-        Scanner scan = new Scanner(System.in);
-        
-        HashMap<String ,Integer> map = new HashMap<>();
-        int count = 0;
-        
-        while(scan.hasNextLine()) {
-            String str = scan.nextLine();
-            map.put(str, map.getOrDefault(str, 0) + 1);
-            count++;
+    public static void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
+        Map<String, Integer> map = new TreeMap<>();
+        int total = 0;
+        String name;
+        while ((name = br.readLine()) != null) {
+            map.put(name, map.getOrDefault(name, 0) + 1);
+            total++;
         }
-        
-        Object[] keyArr = map.keySet().toArray(); // 키 값을 배열로 받아옴
-        Arrays.sort(keyArr); 
-        
-        StringBuilder sb = new StringBuilder();
-        for(int i = 0; i < keyArr.length; i++) {
-            int value = map.get(keyArr[i]);
-            double percent = ((double)value /(double) count) * 100;
-            sb.append(keyArr[i] + " " + String.format("%.4f", percent) + "\n");
+
+        StringBuilder result = new StringBuilder();
+        for (Map.Entry<String, Integer> entry : map.entrySet()) {
+
+            result.append(entry.getKey())
+                    .append(" ")
+                    .append(String.format("%.4f", (double) entry.getValue() / (double) total * 100.0))
+                    .append("\n");
         }
-        System.out.println(sb.toString());
+
+        bw.write(result.toString());
+
+
+        br.close();
+        bw.close();
     }
+    /*public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        Map<String, Integer> map = new TreeMap<>();
+        int total = 0;
+        String name;
+        while ((name = br.readLine()) != null) {
+            map.put(name, map.getOrDefault(name, 0) + 1);
+            total++;
+        }
+
+        for (Map.Entry<String, Integer> entry : map.entrySet()) {
+            double treeRatio = (double)entry.getValue() / (double)total * 100;
+            System.out.println(entry + " " + String.format("%,.4f", treeRatio));
+        }
+    }*/
 }
