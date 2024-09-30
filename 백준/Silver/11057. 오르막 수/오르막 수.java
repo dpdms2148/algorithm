67208c22b0fd0685c60f311edf15ba66755ebca3
dpdms2148 <57666226+dpdms2148@users.noreply.github.com>
2024-09-30 20:39:51 +0900

    [Silver I] Title: 오르막 수, Time: 68 ms, Memory: 11596 KB -BaekjoonHub

---
+++ 백준/Silver/11057. 오르막 수/오르막 수.java
@@ -0,0 +1,33 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int N = Integer.parseInt(br.readLine());
+        int[][] dp = new int[1002][10];
+
+        for (int i = 0; i < 10; i++) {
+            dp[1][i] = 1;
+        }
+        for (int i = 2; i <= N; i++) {
+            for (int j = 0; j < 10; j++) {
+                if (j == 0) {
+                    dp[i][j] = 1;
+                    continue;
+                }
+
+                dp[i][j] = dp[i - 1][j] + dp[i][j - 1];
+                dp[i][j] %= 10007;
+            }
+        }
+
+        int answer = 0;
+        for (int i = 0; i < 10; i++) {
+            answer += (dp[N][i] % 10007);
+        }
+        System.out.println(answer % 10007);
+
+    }
+}
