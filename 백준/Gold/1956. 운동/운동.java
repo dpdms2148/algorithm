ac44c93ef6e830cfcdcd6e14c383cb26aa51665e
dpdms2148 <57666226+dpdms2148@users.noreply.github.com>
2025-06-30 20:57:07 +0900

    [Gold IV] Title: 운동, Time: 876 ms, Memory: 73208 KB -BaekjoonHub

---
+++ 백준/Gold/1956. 운동/운동.java
@@ -0,0 +1,60 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Main {
+    static final int INF = 987654321;
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+        int V = Integer.parseInt(st.nextToken());
+        int E = Integer.parseInt(st.nextToken());
+
+        int[][] map = new int[V + 1][V + 1];
+        for (int i = 1; i <= V; i++) {
+            for (int j = 1; j <= V; j++) {
+                if (i != j) map[i][j] = INF;
+            }
+        }
+
+        for (int i = 0; i < E; i++) {
+            st = new StringTokenizer(br.readLine());
+            int x = Integer.parseInt(st.nextToken());
+            int y = Integer.parseInt(st.nextToken());
+            int c = Integer.parseInt(st.nextToken());
+            map[x][y] = c;
+        }
+
+        for (int k = 1; k <= V; k++) {
+            for (int i = 1; i <= V; i++) {
+                for (int j = 1; j <= V; j++) {
+                    if (i == j) {
+                        continue;
+                    }
+
+                    if (map[i][j] > map[i][k] + map[k][j]) {
+                        map[i][j] = map[i][k] + map[k][j];
+                    }
+                }
+            }
+        }
+
+        int answer = INF;
+        for (int i = 1; i <= V; i++) {
+            for (int j = 1; j <= V; j++) {
+                if (i == j) {
+                    continue;
+                }
+
+                if (map[i][j] != INF && map[j][i] != INF) {
+                    answer = Math.min(answer, map[i][j] + map[j][i]);
+                }
+
+            }
+        }
+        answer = (answer == INF) ? -1 : answer;
+        System.out.println(answer);
+    }
+}
